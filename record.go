9a4103656817edbea673758439216c7732f00d59
Randy Reddig <ydnar@shaderlab.com>
2014-04-28 09:07:05 +0800

    Two structs: - Response for an unparsed whois server response - Record for a parsed response

--- record.go
+++ record.go
@@ -1,47 +1,22 @@
 package whois
 
-import (
-	"errors"
+import ()
+
+// Status summarizes a domain name’s RPP or EPP status
+type Status int
+
+const (
+	Available Status = iota
+	Registered
+	Invalid
 )
 
-// Record represents an individual whois record
-type Record interface {
-	Query() string
-	URL() string
-	ContentType() string
-	Encoding() string
-	Body() []byte
-	String() string
-	Parse() error
+// Record represents a parsed whois response
+type Record struct {
+	Response
+	Status
 }
 
-type recordFields struct {
-	query       string
-	url         string
-	contentType string
-	encoding    string
-	body        []byte
-}
-
-func (record *recordFields) Query() string { return record.query }
-func (record *recordFields) URL() string { return record.url }
-func (record *recordFields) ContentType() string { return record.contentType }
-func (record *recordFields) Encoding() string { return record.encoding }
-func (record *recordFields) Body() []byte { return record.body }
-func (record *recordFields) String() string { return string(record.body) }
-func (record *recordFields) Parse() error { return errors.New("Parse() method not implemented.") }
-
-type baseRecord struct {
-	recordFields
-}
-
-type nrRecord baseRecord
-
-func (record *nrRecord) Parse() error {
-	return nil
-}
-
-func RecordStore() Record {
-	record := &nrRecord{recordFields{query: "domai.nr"}}
-	return record
+func (record *Record) IsRegistered() bool {
+	return record.Status != Available
 }
---
+++ response.go
@@ -0,0 +1,19 @@
+package whois
+
+import (
+	"time"
+)
+
+// Response represents a whois response from a server
+type Response struct {
+	Query string
+	URL string
+	FetchedAt time.Time
+	ContentType string
+	Encoding string
+	Body []byte
+}
+
+func (response *Response) String() string {
+	return string(response.Body)
+}
