277e4cfc5c4b3a72a5484cbdac5dd88c529bf352
Randy Reddig <ydnar@shaderlab.com>
2014-05-26 18:51:28 -0700

    www.whois.az

---
+++ server_az.go
@@ -0,0 +1,28 @@
+package whois
+
+import (
+	"net/url"
+	"strings"
+)
+
+var az = &Server{
+	Resolve: func(req *Request) error {
+		labels := strings.SplitN(req.Query, ".", 2)
+		values := url.Values{}
+		values.Set("lang", "en")
+		values.Set("domain", labels[0])
+		values.Set("dom", labels[1])
+		req.URL = "http://www.whois.az/cgi-bin/whois.cgi"
+		req.Body = values.Encode()
+		req.Referer = "http://www.whois.az/"
+		return nil
+	},
+}
+
+func init() {
+	register(
+		az,
+		"www.whois.az",
+		"www.nic.az",
+	)
+}
