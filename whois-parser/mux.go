9ef1d62f08d4a344f55ec08364e46c020eb0492e
Koala Yeung <koalay@gmail.com>
2018-02-22 11:12:54 +0800

    whois-parser: mux implementation to route query

---
+++ whois-parser/mux.go
@@ -0,0 +1,44 @@
+package parser
+
+// QueryType identify the type of
+// WHOIS query for the parser
+type QueryType int
+
+// Types of WHOIS query
+const (
+	TypeTLD QueryType = iota
+	TypeDomain
+	TypeIPv4
+	TypeIPv6
+	TypeOther
+)
+
+// Mux stores the Parsers against the registrant hostname
+// and can parse a given host, body pair.
+type Mux map[string]Parser
+
+// Set a Parser implementation for a given
+// registrar / registry
+func (m Mux) Set(host string, parser Parser) {
+	m[host] = parser
+}
+
+// ParserOf returns the parser of the given query and host.
+func (m Mux) ParserOf(host string, queryType QueryType) (parser Parser) {
+	var ok bool
+	if parser, ok = m[host]; ok {
+		return
+	}
+	parser, _ = m[""] // use fallback parser, if any
+	return
+}
+
+// DefaultMux returns a Mux with all default parser implementations
+func DefaultMux() *Mux {
+	defaultParser := DefaultMapping()(DefaultParser)
+	return &Mux{
+		"":                       defaultParser,
+		"whois.pir.org":          defaultParser,
+		"whois.verisign-grs.com": defaultParser,
+	}
+}
